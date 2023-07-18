b17a389aab441c1b14f0fc39608a677efcf6a311
infinito81 <33931789+infinito81@users.noreply.github.com>
2023-07-18 09:11:40 +0200

    Add files via upload

---
+++ export_corredores.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (119, 'Carlos', 'Sánchez', '12346785P', 'sccid2s@gmail.com', 'MASC', '2001-07-20', 'CADETE', 'Navalcan', '', 600);
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (120, 'Jorge ', 'Sánchez López', '32165478T', 'sccid2s@gmail.com', 'MASC', '1981-12-21', 'SENIOR', 'navalcan', '', 601);
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (121, 'Nisio', 'Quenos Trincan', '45678912H', 'sccid2s@gmail.com', 'MASC', '1972-12-21', 'VETERANO', 'Gamonal', '', 602);
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (122, 'Pablo', 'Sánchez López', '45678912G', 'sccid2s@gmail.com', 'MASC', '2003-12-21', 'INFANTIL', 'Gamonal', '', 603);
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (123, 'Iván', 'Galán Bardera', '45678912Q', 'sccid2s@gmail.com', 'MASC', '1999-12-21', 'SENIOR', 'Argés', '', 604);
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (124, 'María Mercedes', 'Misena Alvares de Perea', '1234678P', 'sccid2s@gmail.com', 'FEME', '1978-10-26', 'SENIOR', 'Talavera de la Reina', 'UDAT', 605);
+INSERT INTO corredores (id, nombre, apellidos, dni, email, sexo, fechaNac, categoria, poblacion, club, dorsal) VALUES (125, 'ANGELINA', 'Sánchez Nisio', '', '', 'FEME', '2007-10-21', 'BENJAMIN', 'NAVALCÁN', '', 606);
